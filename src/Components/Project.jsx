1d6cf11abac7be35cd0487ffbb744ed65be7a9ee
bharat <bharatkhatwani06@gmail.com>
2025-05-20 15:57:13 +0530

    update the project Section

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -22,11 +22,11 @@ const Projects = () => {
   return (
     <section id="projects" className="max-w-5xl mx-auto px-4 py-12 mt-14">
       <h2 className="text-6xl font-bold text-[#4EC3FF] mb-12 text-center">Projects</h2>
-      <div className="grid gap-8 sm:grid-cols-2 m-8" >
+      <div className="grid gap-8 sm:grid-cols-2 m-8">
         {projects.map(({ title, description, tech, live, github }, index) => (
           <div
             key={index}
-            className="group bg-[#121212] rounded-lg p-6 shadow-md hover:shadow-xl transform hover:scale-105 transition duration-300 ease-in-out"
+            className="group bg-[#121212] rounded-lg p-6 shadow-md border border-[#2c2c2c] hover:border-[#4EC3FF] hover:shadow-xl transform hover:scale-105 transition duration-300 ease-in-out"
           >
             {/* Project Title */}
             <h3 className="text-xl font-semibold text-white group-hover:text-[#4EC3FF] mb-8">
